291409f50d2b977b8f802c76c679ddd7af0ac9f5
Whoknowsme0nobody <belm8582@gmail.com>
2026-01-17 15:48:58 +0100

    Initialize React application with SmoothRatioExplorer

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SmoothRatioExplorer from './SmoothRatioExplorer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<SmoothRatioExplorer />);
